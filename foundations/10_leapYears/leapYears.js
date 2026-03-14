74175bf91cf72549cf597ca5d10953f56bc1888d
Sana <SanaAlmatrafi@gmail.com>
2026-03-14 09:48:54 +0300

    add solution to Exercise 10 - leapYears

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
+const leapYears = function (year) {
 
+    let divisibleByFour = (year % 4 === 0);
+    let divisibleBy100 = (year % 400 === 0);
+    if (year % 10 === 0) {
+        return (divisibleByFour && divisibleBy100);
+    }
+    return divisibleByFour;
 };
 
 // Do not edit below this line
