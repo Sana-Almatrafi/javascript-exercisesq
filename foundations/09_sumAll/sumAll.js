7ea6bbb0446fa5d7bace8fe2ee2a2857846e47c0
Sana <SanaAlmatrafi@gmail.com>
2026-03-14 09:35:23 +0300

    add solution to Exercise 09 - sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+    if (!Number.isInteger(num1)
+        || !Number.isInteger(num2)
+        || num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+    let resultSum = 0;
+    let big = num2;
+    let small = num1;
+    if (num1 > num2) {
+        big = num1;
+        small = num2;
+    }
+    for (let i = small; i <= big; i++) {
+        resultSum +=  i;
+    }
+    return resultSum;
 };
 
 // Do not edit below this line
