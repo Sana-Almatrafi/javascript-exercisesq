b3edc1f5ad2ba8f2b77fa596f32408a8f4259668
Sana <SanaAlmatrafi@gmail.com>
2026-03-14 07:52:38 +0300

    add solution to Exercise 08 - removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+
+    let resultArr = [];
+
+    for(let item of arr){
+        if(args.includes(item)){
+            continue;
+        }
+        resultArr.push(item);
+    }
+    return resultArr;
+
 };
 
 // Do not edit below this line
